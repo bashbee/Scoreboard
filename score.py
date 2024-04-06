aa1cefc08c286259ad0197dcbf2d008057105c12
bashbee <bashbee91@gmail.com>
2024-04-05 23:13:37 -0500

    Create score.py

---
+++ score.py
@@ -0,0 +1,26 @@
+import ScoreboardUtil
+import logging
+import time
+
+royalsTeamId = str("118")
+
+logging.basicConfig(filename='score.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(levelname)s %(message)s',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger()
+
+print(ScoreboardUtil.time_until_next_mlb_game(royalsTeamId))
+#while(1==1):
+ #   gameToday = ScoreboardUtil.nextGame(royalsTeamId)
+ #   print(gameToday)
+ #   logger.log(logging.INFO, "Game today " + str(gameToday))
+ #   time.sleep(10)
+    #poll every hour until game is less than an hour from now
+    #check for a game today
+
+
+    #handle double headers
+
+    #after the game, sleep for the rest of the day
